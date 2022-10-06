a470d8e0d7ef321b2bacc273a9e4e636401c16bc
ayu0616 <ayumuogawa0616@gmail.com>
2022-10-06 10:10:39 +0000

    create settings.py

---
+++ python/settings.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv(verbose=True)
+
+dotenv_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env")
+load_dotenv(dotenv_path)
+
+USER_NAME = os.environ.get("USER_NAME")
+PASSWORD = os.environ.get("PASSWORD")
