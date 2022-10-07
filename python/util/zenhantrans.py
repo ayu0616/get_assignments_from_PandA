d424e9511d36a846a2dd2db7619c9bc845b14082
ayu0616 <ayumuogawa0616@gmail.com>
2022-10-07 09:20:40 +0900

    数字限定の関数を追加

--- python/util/zenhantrans.py
+++ python/util/zenhantrans.py
@@ -1,6 +1,9 @@
 ZEN = "".join(chr(0xFF01 + i) for i in range(94))
 HAN = "".join(chr(0x21 + i) for i in range(94))
 
+ZEN_NUMBER = "０１２３４５６７８９"
+HAN_NUMBER = "0123456789"
+
 ZEN2HAN = str.maketrans(ZEN, HAN)
 HAN2ZEN = str.maketrans(HAN, ZEN)
 
@@ -11,3 +14,7 @@ def zen2han(s: str):
 
 def han2zen(s: str):
     return s.translate(HAN2ZEN)
+
+
+def zen2han_number(s: str):
+    return s.translate(str.maketrans(ZEN_NUMBER, HAN_NUMBER))
