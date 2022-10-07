38e2e4ef5780fe921d272b845052d27431ec6dcf
ayu0616 <ayumuogawa0616@gmail.com>
2022-10-07 19:00:30 +0900

    移動しただけ

--- python/types/task.py
+++ python/types/task.py
@@ -3,7 +3,8 @@ from typing import TypedDict
 
 class Task(TypedDict):
     title: str
+    class_name: str
     panda_id: str
     due_date: str
-    description: str
-    last_fixed_date: str
+    url: str
+    last_fixed_date: str  # 最終更新日時（自分側の）
