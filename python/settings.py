e84e4518bc0ee48a0433afada1c5809a11c83d23
ayu0616 <ayumuogawa0616@gmail.com>
2022-10-08 16:58:03 +0900

    スプレッドシートに送信するためのURLを追加

--- python/settings.py
+++ python/settings.py
@@ -9,5 +9,7 @@ load_dotenv(dotenv_path)
 
 USER_NAME = os.environ.get("USER_NAME")
 PASSWORD = os.environ.get("PASSWORD")
+GOOGLE_SPREADSHEET_POST_URL = os.environ.get("GOOGLE_SPREADSHEET_POST_URL")
+
 
 NOW_SEMESTER = "2022後期"
